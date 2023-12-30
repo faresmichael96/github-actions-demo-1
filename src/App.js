fbe4d557f42f14c6f74fd2b9c6fbab1b12afe9a3
Michael Fares <fares_michael@live.com>
2023-12-30 18:41:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>How to Deploy a Frontend React App on Vercel in Minutes</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=FvsvHzcwOmQ"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">
+          check this
         </a>
       </header>
     </div>
